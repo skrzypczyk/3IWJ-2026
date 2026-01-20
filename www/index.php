c781ca163efff7993e7ba727fce869f6a04d2925
Yves SKRZYPCZYK <y.skrzypczyk@gmail.com>
2026-01-20 11:43:00 +0100

    first commit

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="la description de ma page">
+        <title>Titre de la page</title>
+    </head>
+    <body>
+
+    <?php
+
+        //Commentaire sur une ligne
+        /*
+            Commentaire sur plusieurs lignes
+        */
+
+            
+    ?>
+
+    </body>
+</html>
