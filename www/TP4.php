818f3f8c68c5f6ef488503d069633c545e1376f0
Yves SKRZYPCZYK <y.skrzypczyk@gmail.com>
2026-01-22 17:29:07 +0100

    TP login

---
+++ www/TP4.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Envoyer le fichier TP3 et TP4 par mail : y.skrzypczyk@gmail.com
+Objet : TP login - 3IWJ - 2026 - Nom prenom
+
+Objectif :
+    - Unicté de l'email - 4 points
+    - Redirection sur TP4.php si inscription ok - 2 points
+    - Faire une page de login dans TP4 avec vérification des identifiants  - 2 points
+    - Garder la connextion persistante - 3 points
+    - Si connecté ne pas afficher le formulaire de login mais un bouton de déconnexion et la liste de tous les utilisateurs inscrits dans un tableau  - 5 points
+    - Ajouter un bouton de suppression par user qui fonction en restant sur le fichier TP4.php  - 4 points
+    - Attention à la sécurité
+*/
